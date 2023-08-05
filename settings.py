1656c3234575c822088ea1357f0b3856fb094984
azuckerman <AMZ58@pitt.edu>
2023-08-04 23:04:44 -0400

    add_settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """"This is the class to store all settings for the invasion."""
+
+    def __init__(self):
+
+        #Screen display settings L X W X H
+
+        self.width = 1200
+        self.height = 800
+        self.color = (100,230,230)
+        
